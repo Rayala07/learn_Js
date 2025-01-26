20dd6bddf3e32619aa59cd232283939ac7c48f0e
Rayala07 <167209095+Rayala07@users.noreply.github.com>
2025-01-26 09:48:46 +0000

    Updated

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,3 +12,17 @@ true => 1 false => 0
 //Conversion is important to know ,, bcz users enter different types of data in the browser so to know the dataType & also sometimes convert it.
 
 
+// *** Operation *** 
+
+let str1 = "Hello"
+let str2 = " Rayala"
+let ans = str1 + str2
+console.log(ans)
+
+console.log("1" + 2) //=> 12
+console.log("1" + 2 + 2) //=> 122
+console.log(1 + 2 + "3") //=> 33
+
+//Prefix and Postfix Operator is important to learn - resource MDN.
+
+
