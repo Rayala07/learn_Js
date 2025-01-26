f0c33891b75a565fa15b43c1c388a98baf5a3de3
Rayala07 <167209095+Rayala07@users.noreply.github.com>
2025-01-26 10:12:04 +0000

    Comparing values

---
+++ 01_basics/comparsion.js
@@ -0,0 +1,13 @@
+
+/*
+<
+> 
+<=
+>=
+These all are common and easily understandable , they return Boolean result 
+
+*/
+
+console.log("2" == 2) //=> will return "True" bcz == converts the dataType if not same and returns the from the value only.
+
+console.log("3" === 3) //=> will return "False" bcz === compares the 'Value' & 'DataType' both before returning the result.
