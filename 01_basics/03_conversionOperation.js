f919326b7c3eec19b74d15acae40ec2d3952aae8
Rayala07 <167209095+Rayala07@users.noreply.github.com>
2025-01-26 09:26:35 +0000

    DataTypes Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+/*
+Datatype's name => Number , String . Booleanetc.
+Number(var_name)
+
+What it displays while printing typeof() ? 
+
+"33" => 33
+"33abc" => NaN
+true => 1 false => 0
+*/
+
+//Conversion is important to know ,, bcz users enter different types of data in the browser so to know the dataType & also sometimes convert it.
+
+
