c68f08c64c34f2eae7cb882859249af42d15dc6b
Rayala07 <167209095+Rayala07@users.noreply.github.com>
2025-01-26 09:11:18 +0000

    Updates

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144532
+let accountEmail = "rayala@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur" //Should not use this standard
+
+// accountId = 23 (Not allowed)
+
+/*
+Using var is not needed , use let to define modifiable variable!
+*/
+
+//Printing
+console.log(accountId);
+
+//Printing multiple values , displays data in tabular format! 
+console.table([accountEmail, accountPassword, accountCity])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //Treats whole JS code as newer ver.
+
+/*
+; this is avoided to increase Readabillity , you can use it also (optional). 
+*/
+
+let name = "Me" //String
+let age = 22 //Integer
+let isLogged = true //boolean
+
+//number => 2 to power 53 range of the number
+//bigint (stockmarket for largest numbers)
+//null => standalone value / representing empty / object type it is.
+//undefined => when any value is not defined to any var. / it is also type - undefined type
+//symbol => unique
+
+//to know typeOf the Data present..
+console.log(typeof "", 2, null);
