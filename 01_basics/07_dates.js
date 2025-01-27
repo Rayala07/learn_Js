025e5ae6a5cdd9fc1d884c8f7b6040aca072178b
Rayala07 <167209095+Rayala07@users.noreply.github.com>
2025-01-27 09:10:53 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,8 @@
+// Dates in RealTime 
+// These methods return current date & time in different formats
+
+let myDate = new Date()
+
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())   
