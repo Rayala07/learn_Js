028aae608211eda58f00cd5564e4d7a39790ad95
Rayala07 <167209095+Rayala07@users.noreply.github.com>
2025-01-31 08:37:57 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,11 @@
+// For loop 
+
+// Break & Continue :- 
+
+for(let i=1; i<=10; i++){
+    if(i == 5){
+        console.log(`Detected ${i}`);
+        //continue     //break
+    }
+    console.log(i);
+}
